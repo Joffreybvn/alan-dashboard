9e4876380fc9bbf468df322d2bcc1becec72a9ff
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-03-03 17:47:42 +0100

    redirections disabled

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ export function main() {
         applySettings(settings)
 
     }).catch((message) => {
-        document.location.href= SITE_URL + "error.html"
+        //document.location.href= SITE_URL + "error.html"
     })
 
     // Triggered when the user save
